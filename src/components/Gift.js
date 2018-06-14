4185fe103b68e80ad3d5e6856bb24a7ca4263015
Chase Jensen <soccerchase@gmail.com>
2018-06-14 14:45:56 -0600

    Custom styling

--- src/components/Gift.js
+++ src/components/Gift.js
@@ -9,7 +9,7 @@ class Gift extends Component {
   }
   render() {
     return (
-      <div>
+      <div className="gift">
         <Form>
           <FormGroup>
             <ControlLabel>Person</ControlLabel>
