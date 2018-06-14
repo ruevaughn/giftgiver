49e9cc3c72e1296c0bbe108ea1f9ca4a948ad803
Chase Jensen <soccerchase@gmail.com>
2018-06-13 23:41:49 -0600

    have to tweak tests to get them to pass - test pollution

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -2,19 +2,28 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
 
-const app = shallow(<App />);
+describe('App', () => {
+  const app = shallow(<App />);
 
-it('renders correctly', () => {
-  expect(app).toMatchSnapshot();
-});
+  it('renders correctly', () => {
+    expect(app).toMatchSnapshot();
+  });
 
-it('initializes the `state` with an empty list of gifts', () => {
-  expect(app.state().gifts).toEqual([]);
-});
+  it('initializes the `state` with an empty list of gifts', () => {
+    expect(app.state().gifts).toEqual([]);
+  });
 
-it('adds a new gift to `state` when clicking the `add gift` button', () => {
-  // find by jsx or class name.
-  app.find('.btn-add').simulate('click');
+  it('adds a new gift to `state` when clicking the `add gift` button', () => {
+    // find by jsx or class name.
+    app.find('.btn-add').simulate('click');
 
-  expect(app.state().gifts).toEqual([{id: 1}]);
+    expect(app.state().gifts).toEqual([{id: 1}]);
+  });
+
+  it('adds new gift to the rendered list when clicking the `add gift` button', () => {
+    app.find('.btn-add').simulate('click');
+
+    expect(app.find('.gift-list').children().length).toEqual(2);
+  });
+
 });
