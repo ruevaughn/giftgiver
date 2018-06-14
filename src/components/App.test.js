6570f0ce6ab21af97a627426e5925b51ae123841
Chase Jensen <soccerchase@gmail.com>
2018-06-13 23:43:01 -0600

    Deal with BDD Standard test pollution as you would - break up into scenarios, clean up database to an unaltered clean state before running assertions and tests

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -13,17 +13,23 @@ describe('App', () => {
     expect(app.state().gifts).toEqual([]);
   });
 
-  it('adds a new gift to `state` when clicking the `add gift` button', () => {
-    // find by jsx or class name.
-    app.find('.btn-add').simulate('click');
+  describe('when clicking the `add-gift` button', () => {
+    beforeEach(() => {
+      // find by jsx or class name.
+      app.find('.btn-add').simulate('click');
+    });
 
-    expect(app.state().gifts).toEqual([{id: 1}]);
+    afterEach(() => {
+      app.setState({ gifts: [] });
+    });
+
+    it('adds a new gift to `state`', () => {
+      expect(app.state().gifts).toEqual([{id: 1}]);
+    });
+
+    it('adds new gift to the rendered list', () => {
+      expect(app.find('.gift-list').children().length).toEqual(1);
+    });
+
   });
-
-  it('adds new gift to the rendered list when clicking the `add gift` button', () => {
-    app.find('.btn-add').simulate('click');
-
-    expect(app.find('.gift-list').children().length).toEqual(2);
-  });
-
 });
