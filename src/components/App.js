7eec3c055fb1c5b79a66b14a81984213a9d95fa8
Chase Jensen <soccerchase@gmail.com>
2018-06-13 23:15:31 -0600

    No need to install jest-cli wiht npm anymore

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
 
 class App extends Component {
   constructor(){
@@ -6,10 +7,26 @@ class App extends Component {
 
     this.state = {gifts: [] };
   }
+
+  addGift = () => {
+    const { gifts } = this.state;
+
+    const ids = this.state.gifts.map(gift => gift.id);
+
+    // for each of the ids do [0] [1] [2] etc.
+    const max_id = ids.lengths > 0 ? Math.max(...ids) : 0;
+
+    gifts.push({id: max_id+1});
+
+    this.setState({ gifts });
+
+  }
+
   render() {
     return (
       <div>
         <h2>Gift Giver</h2>
+        <Button className="btn-add" onClick={this.addGift}>Add Gift</Button>
       </div>
     )
   }
--- src/components/App.test.js
+++ src/components/App.test.js
@@ -11,3 +11,10 @@ it('renders correctly', () => {
 it('initializes the `state` with an empty list of gifts', () => {
   expect(app.state().gifts).toEqual([]);
 });
+
+it('adds a new gift to `state` when clicking the `add gift` button', () => {
+  // find by jsx or class name.
+  app.find('.btn-add').simulate('click');
+
+  expect(app.state().gifts).toEqual([{id: 1}]);
+});
