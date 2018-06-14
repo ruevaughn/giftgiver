a8b5be0d60bcac0f511fd9d27565947306dc5812
Chase Jensen <soccerchase@gmail.com>
2018-06-14 00:04:49 -0600

    Gift Person Test with duplication

--- src/components/Gift.test.js
+++ src/components/Gift.test.js
@@ -8,4 +8,17 @@ describe('Gift', () => {
   it('renders properly', () => {
     expect(gift).toMatchSnapshot();
   });
+
+  describe('when typing into the person input', () => {
+    beforeEach(() => {
+      // On Change rather than click simulation you have to tell the change function what you want to change it to. Since this is simulating
+      // A user changing an input field. In the DOM behavior, when changed, inputs fire the event object which contains the target object
+      // which holds the target object.
+      gift.find('.input-person').simulate('change', { target: {value: 'Uncle' } });
+    });
+
+    it('updates the person in `state`', {} => {
+      expect(gift.state(person).toEqual('Uncle');
+    });
+  });
 });
