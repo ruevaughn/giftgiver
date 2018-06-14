65b7ab3b8afb7eda7959d28433785eda870abc5e
Chase Jensen <soccerchase@gmail.com>
2018-06-13 21:37:16 -0600

    Update jest snapshop and remove pack-lock.json as well as yarn.lock, then re-run yarn install and npm install to get rid of random error 'TypeError: environment.teardown is not a function at node_modules/jest-runner/build/run_test.js'

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Gift Giver</h2>
+      </div>
+    )
+  }
+}
+
+export default App;
