9c61a006156fd2545504002a025ebbcde17d3aa9
Chase Jensen <soccerchase@gmail.com>
2018-06-13 21:45:28 -0600

    Test for setting state

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {gifts: [] };
+  }
   render() {
     return (
       <div>
--- src/components/App.test.js
+++ src/components/App.test.js
@@ -7,3 +7,7 @@ const app = shallow(<App />);
 it('renders correctly', () => {
   expect(app).toMatchSnapshot();
 });
+
+it('initializes the `state` with an empty list of gifts', () => {
+  expect(app.state().gifts).toEqual([]);
+});
