4fa531a0db0bba2c3ba429882b243152222ceaa8
Chase Jensen <soccerchase@gmail.com>
2018-06-14 00:38:33 -0600

    Refactor Test and add matching component

--- src/components/Gift.js
+++ src/components/Gift.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
+import { Form, FormGroup, FormControl, ControlLabel, Button } from 'react-bootstrap';
 
 class Gift extends Component {
+  constructor() {
+    super();
+
+    this.state = {person: '', present: '' };
+  }
   render() {
     return (
-      <div></div>
+      <div>
+        <Form>
+          <FormGroup>
+            <ControlLabel>Person</ControlLabel>
+            <FormControl className='input-person' onChange={event => this.setState({ person: event.target.value })}
+            />
+          </FormGroup>
+        </Form>
+      </div>
     );
   }
 }
--- src/components/Gift.test.js
+++ src/components/Gift.test.js
@@ -10,15 +10,17 @@ describe('Gift', () => {
   });
 
   describe('when typing into the person input', () => {
+    const person = "Uncle";
+
     beforeEach(() => {
       // On Change rather than click simulation you have to tell the change function what you want to change it to. Since this is simulating
       // A user changing an input field. In the DOM behavior, when changed, inputs fire the event object which contains the target object
       // which holds the target object.
-      gift.find('.input-person').simulate('change', { target: {value: 'Uncle' } });
+      gift.find('.input-person').simulate('change', { target: { value: person } });
     });
 
-    it('updates the person in `state`', {} => {
-      expect(gift.state(person).toEqual('Uncle');
+    it("updates the person in `state`", () => {
+      expect(gift.state().person).toEqual(person);
     });
   });
 });
